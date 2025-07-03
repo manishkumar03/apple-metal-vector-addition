316207c4b675c80db5ae9fc74959ee7258fa6afd
Manish Kumar <manish.bansal@gmail.com>
2025-07-03 14:51:50 -0400

    Initial Commit

---
+++ AppleMetalVectorAddition/AppleMetalVectorAdditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMetalVectorAdditionApp.swift
+//  AppleMetalVectorAddition
+//
+//  Created by Manish Kumar on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct AppleMetalVectorAdditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMetalVectorAddition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMetalVectorAddition
+//
+//  Created by Manish Kumar on 2025-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
